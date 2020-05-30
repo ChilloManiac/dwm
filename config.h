91fcd329979bf11a5ccf1d1c754125ed9fa0d8b6
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-05-30 10:02:06 +0200

    get status2d

--- config.h
+++ config.h
@@ -12,11 +12,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FontAwesome5FreeSolid:size=14","FiraCode:size=12"};
 static const char dmenufont[]       = "FiraCode:size=12";
-//static const char col_gray1[]       = "#222222";
-//static const char col_gray2[]       = "#444444";
-//static const char col_gray3[]       = "#bbbbbb";
-//static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#005577";
 static const char col_bg[]          = "#282828";
 static const char col_fg[]          = "#ebddd2";
 static const char col_brd[]         = "#fe8019";
